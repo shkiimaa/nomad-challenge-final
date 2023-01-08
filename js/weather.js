dbb1094b72ffed53d560533ec8f9025e89ad7fd2
shkiimaa <shkiimaa@gmail.com>
2023-01-09 02:17:16 +0900

    날씨 기능 완성

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = 'a409ed029f4dc038db2d0acb5a5662e2';
+
+function onGeoThen(positon) {
+  const lat = positon.latitude;
+  const lon = positon.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  fetch(url).then((response) =>
+    response.json().then((data) => {
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    })
+  );
+}
+
+function onGeCatch() {
+  alert("Can't find you. No weater for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoThen, onGeCatch);
